2f99756a25dd9517fa5ba0855804a0076e6fb039
kopringo <kopringo@gmail.com>
2017-12-21 00:25:30 +0100

    cluster test, static filex

--- GenealogyMaps/urls.py
+++ GenealogyMaps/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .apps import core
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('GenealogyMaps.apps.core.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
