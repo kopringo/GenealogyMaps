4b731efea70e1d8bc216395bf29eb251196d3536
kopringo <kopringo@gmail.com>
2018-12-31 02:46:57 +0100

    experiments with django rest framework

--- GenealogyMaps/apps/core/urls.py
+++ GenealogyMaps/apps/core/urls.py
@@ -1,14 +1,14 @@
 
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url, include
 from . import views
 from . import views_api
 
-from .urls_api import router
+from . import urls_api
 
 urlpatterns = [
     #path('api/parishes', views_api.parishes__get),
-    url('api2', include(router.urls)),
+    re_path(r'^api2/', include(urls_api)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', views.home)
 ]
--- GenealogyMaps/apps/core/urls_api.py
+++ GenealogyMaps/apps/core/urls_api.py
@@ -1,4 +1,9 @@
-from rest_framework import routers, serializers, viewsets
+
+from django.urls import path, re_path
+from django.conf.urls import url, include
+
+from rest_framework import routers, serializers, viewsets, generics
+from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer, AdminRenderer, BrowsableAPIRenderer
 
 from .models import Parish
 
@@ -10,12 +15,33 @@ class ParishSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'name', 'geo_lat', 'geo_lng')
 
 
-# ViewSets define the view behavior.
-class ParishViewSet(viewsets.ModelViewSet):
+class ParishDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parish
+        fields = ('id', 'name', 'year', 'country', 'province', 'county', 'place', 'postal_code', 'postal_place', 'address', 'diocese', 'deanery')
+
+
+class ParishList(generics.ListAPIView):
     queryset = Parish.objects.all()
     serializer_class = ParishSerializer
 
+    """
+    def get_queryset(self):
+        username = self.kwargs['username']
+        return Purchase.objects.filter(purchaser__username=username)
+    """
+
+
+class ParishDetails(generics.RetrieveAPIView):
+    queryset = Parish.objects.all()
+    serializer_class = ParishDetailSerializer
+    renderer_classes = (BrowsableAPIRenderer, JSONRenderer, AdminRenderer, TemplateHTMLRenderer)
+    template_name = 'core/api/parish_detail.html'
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'/parishes', ParishViewSet)
+#router = routers.DefaultRouter()
+#router.register(r'/parishes', ParishList.as_view())
+urlpatterns = [
+    re_path(r'^parishes/$', ParishList.as_view()),
+    path('parishes/<int:pk>/', ParishDetails.as_view()),
+]
