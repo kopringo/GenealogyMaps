3cf1f36b64bc3c554d3d57c551a662484bb4f47e
kopringo <kopringo@gmail.com>
2019-01-16 01:24:10 +0100

    document form, experimets with map

--- GenealogyMaps/apps/core/forms.py
+++ GenealogyMaps/apps/core/forms.py
@@ -5,9 +5,20 @@ from .models import DocumentGroup
 
 
 class DocumentGroupForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(DocumentGroupForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name in ('type_b', 'type_m', 'type_d', 'type_a'):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+            if field_name in ('date_excepts', 'note'):
+                field.widget.attrs['rows'] = '3'
+
     class Meta:
         model = DocumentGroup
-        fields = ['name', 'url', 'type', 'type_b', 'type_d', 'type_m', 'type_a', 'date_from', 'date_to', 'date_excepts', 'note']
+        fields = ['name', 'url', 'type', 'type_b', 'type_d', 'type_m', 'type_a', 'date_from', 'date_to', 'date_excepts', 'note', 'source']
 
 
 class CommentForm(forms.Form):
