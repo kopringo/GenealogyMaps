473f8890c7dfe843dc726e17a2bb1621c3d9c56a
kopringo <kopringo@gmail.com>
2019-01-18 23:48:54 +0100

    new importers

--- data/import_lubgens.sh
+++ data/import_lubgens.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 # view-source:http://mapy.lubgens.eu/
+# uniq key:
+# - nazwa miejscowosci + wsp. uproszczone.
+#
+# search:
+# - wspolrzedne geo.
 
 FORCE=0
 
 if [[ ! -f cache/lubgens_root ]] || [[ "$FORCE" == "1" ]]; then
-    wget "http://mapy.lubgens.eu/" -O cache/lubgens_root;
+    wget "https://mapy.lubgens.eu/" -O cache/lubgens_root --header "User-Agent: Mozilla/5.0";
 fi
+
+for i in `grep "var R" cache/lubgens_root -n | awk '{print $1}' | sed -e 's/://g'`; do
+    cat cache/lubgens_root | tail -n +$i | head -n 11 > cache/lubgens_single;
+    cat cache/lubgens_single;
+    echo "===============================";
+
+
+done
---
+++ data/import_szukajwarchiwach.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# view-source:http://mapy.lubgens.eu/
+# uniq key:
+# - numer zespołu
+#
+# search:
+# - recznie...
+
+# https://szukajwarchiwach.pl/4/str/1/100?sort=tytul#tabZasoby
